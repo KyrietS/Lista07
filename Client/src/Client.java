d5ca638cfbdf5a48d13628b9072e1d419662349a
Sebastian Fojcik <fojcik.sebastian@gmail.com>
2018-05-20 01:33:51 +0200

    Dodano architekturę klient-serwer

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,7 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
 public class Client
 {
+    private int port = 4444;
+    private String host = "localhost";
+    private Socket socket = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+
+
+    private Client()
+    {
+        boolean connected = false;
+        System.out.println( "Uruchamianie klienta..." );
+        do
+        {
+            try
+            {
+                socket = new Socket( host, port );
+                out = new PrintWriter( socket.getOutputStream(), true );
+                in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
+                System.out.println( "Połączono z serwerem " + socket.toString() );
+                connected = true;
+            }
+            catch( UnknownHostException e )
+            {
+                printError( "Nieznany host '" + host + "'" );
+                System.exit( -1 );
+            }
+            catch( Exception e )
+            {
+                printError( "Nie można połączyć z serwerem!" );
+                connected = false;
+                System.out.println("Próba połączenia...");
+                try
+                {
+                    TimeUnit.SECONDS.sleep( 3 );
+                }
+                catch( InterruptedException ignored ){}
+            }
+        } while( !connected );
+    }
+
+    private void start()
+    {
+        String line;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        while( true )
+        {
+            try
+            {
+                System.out.print( "Zapytanie: " );
+                line = input.readLine();
+                if( line.equals( "q" ) )
+                    break;
+                out.println( line );
+            }
+            catch( Exception e )
+            {
+                printError( "Problem z zapisem / odczytem ze strumienia" );
+                continue;
+            }
+            try
+            {
+                line = in.readLine();
+                System.out.println( "Odpowiedź: " + line );
+            }
+            catch( IOException e )
+            {
+                printError( "Nie można uzyskać odpowiedzi" );
+            }
+        }
+
+        stop();
+    }
+
+    private void stop()
+    {
+        System.out.println( "Wyłączanie klienta..." );
+        System.exit( 0 );
+    }
+
+    private void printError( String errorMessage )
+    {
+        System.out.println( "[Wystąpił błąd]: " + errorMessage );
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Client..." );
+        Client client = new Client();
+        client.start();
     }
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,41 +1,109 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server
 {
-    private ServerSocket server;
+    private int port = 4444;
+    private ServerSocket server = null;
+    private Socket client = null;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+    private String line = "";
+    private CmdInterpreter command = new CmdInterpreter();
 
     private Server()
     {
         try
         {
-            server = new ServerSocket( 4444 );
+            System.out.println( "Uruchamianie serwera..." );
+            server = new ServerSocket( port );
         }
         catch( IOException e )
         {
-            System.out.println( "Błąd przy tworzeniu serwera!" );
+            printError( "Nie można uzyskać dostępu do portu " + port );
+            stop();
         }
 
     }
 
     private void start()
     {
-        System.out.println("Uruchomiono serwer");
         System.out.println( server.toString() );
+        do
+        {
+            try
+            {
+                System.out.println( "Oczekiwanie na klienta..." );
+                client = server.accept();
+                System.out.println( "Połączono z klientem " + client.toString() );
+            }
+            catch( IOException e )
+            {
+                printError( "Nieudana próba podłączenia klienta na porcie " + port );
+                stop();
+            }
+            try
+            {
+                in = new BufferedReader( new InputStreamReader( client.getInputStream() ) );
+                out = new PrintWriter( client.getOutputStream(), true );
+            }
+            catch( IOException e )
+            {
+                printError( "Nieudana próba podłączenia klienta na porcie " + port );
+                stop();
+            }
+        }while( listenSocket() );
+
         stop();
+    }
+
+    private boolean listenSocket()
+    {
+        try
+        {
+            while( !line.equals( "0" ) )
+            {
+                line = in.readLine();
+                System.out.println( "Otrzymano zapytanie: " + line );
+                line = command.execute( line );
+                out.println( line );
+            }
+        }
+        catch( Exception e )
+        {
+            printError( "Utracono połączenie z klientem" );
+            return true;
+        }
+
+        return false;
     }
 
     private void stop()
     {
         try
         {
+            System.out.println( "Wyłączanie serwera..." );
+            out.close();
+            in.close();
             server.close();
-            System.out.println( "Serwer skończył pracę" );
+            client.close();
+
+            System.exit( 0 );
         }
         catch( IOException e )
         {
             System.out.println( "Błąd przy wyłączaniu serwera!" );
+            System.exit( -1 );
         }
+    }
+
+    private void printError( String errorMessage )
+    {
+        System.out.println( "[Wystąpił błąd]: " + errorMessage );
     }
 
     public static void main( String[] args )
